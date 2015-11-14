d596270db82cbd712113277a4ada5ef6fd2a6a49
Jorge Juan <hi.jorgejuanp@gmail.com>
2015-11-14 09:18:25 +0100

    Iteration 4: Add a memory feature

--- server.rb
+++ server.rb
@@ -11,11 +11,12 @@ post "/calculate" do
                               params[:second_number].to_f,
                               params[:operation])
   result = calculator.calculate
-  "#{result}"
-  # redirect to("/calculation_result")
+  File.open("results.txt", "w") {|file| file.puts "#{result}"}
+  redirect to("/calculation_result")
 end
 
 get "/calculation_result" do
-  calculator.print_result
+  #how to print the result here?
+  @result = File.open("results.txt", "r") {|file| file.read}
   erb(:calculation_result)
 end
